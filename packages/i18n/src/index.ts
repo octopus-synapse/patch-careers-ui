f50deea59ffe1dff49b5d81aa76e12442d2310be
Enzo Ferracini <enzo.patti@aluno.senai.br>
2026-06-09 13:25:04 -0300

    refactor(i18n): kebab-case the pt-BR dictionary filename
    Rename dictionaries/pt-BR.ts -> pt-br.ts to satisfy the kebab-case
    filename convention; the exported locale and `ptBR` binding are unchanged.

--- packages/i18n/src/index.ts
+++ packages/i18n/src/index.ts
@@ -1,5 +1,5 @@
 export { en } from "./dictionaries/en";
-export { ptBR } from "./dictionaries/pt-BR";
+export { ptBR } from "./dictionaries/pt-br";
 export * from "./format";
 export * from "./translator";
 export * from "./types";
